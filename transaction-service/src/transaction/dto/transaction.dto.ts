e5882aa31af698f6e374a945a98cd88ec7644723
Artem Petrenko <artem7888@gmail.com>
2026-05-02 16:13:28 +0300

    fix: add class-transformer decorators to TransactionDto

--- transaction-service/src/transaction/dto/transaction.dto.ts
+++ transaction-service/src/transaction/dto/transaction.dto.ts
@@ -1,20 +1,23 @@
+import { Transform, Type } from 'class-transformer';
 import {
   IsOptional,
   IsString,
   IsNumber,
   IsDateString,
-  IsUUID,
 } from 'class-validator';
 
 export class TransactionDto {
   @IsOptional()
   @IsString()
+  @Transform(({ value }) => (value ? String(value) : undefined))
   transactionId?: string;
 
   @IsString()
   userId: string;
 
   @IsNumber()
+  @Type(() => Number)
+  @Transform(({ value }) => Number(value))
   amount: number;
 
   @IsString()
