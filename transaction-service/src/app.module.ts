5d118b6a4edd1e671b013759308e0cb21cf356e4
Artem Petrenko <artem7888@gmail.com>
2026-05-02 16:03:44 +0300

    prisma and config

--- transaction-service/src/app.module.ts
+++ transaction-service/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,7 +7,14 @@ import { BatchModule } from './batch/batch.module';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule, BatchModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    BatchModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- transaction-service/src/prisma/prisma.service.ts
+++ transaction-service/src/prisma/prisma.service.ts
@@ -1,17 +1,15 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super({ adapter });
-  }
-
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
