ab1185f414786d100635ccc987ffffa0abaa047e
Artem Petrenko <artem7888@gmail.com>
2026-05-04 18:43:30 +0300

    batch-response dto fix

--- transaction-service/src/batch/dto/batch-response.dto.ts
+++ transaction-service/src/batch/dto/batch-response.dto.ts
@@ -1,19 +1,6 @@
-import { Transform, TransformFnParams } from 'class-transformer';
-import { Batch } from '@prisma/client';
-
 export class BatchResponseDto {
-  @Transform((params: TransformFnParams): string => {
-    const batch = params.obj as Batch;
-    return batch.id;
-  })
-  batchId: string;
-
-  @Transform((params: TransformFnParams): string => {
-    const batch = params.obj as Batch;
-    return batch.name;
-  })
-  batchName: string;
-
+  id: string;
+  name: string;
   status: string;
   total: number;
   processed: number;
