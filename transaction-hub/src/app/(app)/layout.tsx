ae3ba445aa2ab2cfa44e4ff3ca6cd76dc51539ee
Artem Petrenko <artem7888@gmail.com>
2026-05-07 14:45:41 +0300

    root layout is updated

--- transaction-hub/src/app/(app)/layout.tsx
+++ transaction-hub/src/app/(app)/layout.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import LoggedInLayout from "@/src/components/LoggedInLayout";
+import Box from "@/src/components/Box";
+import Header from "@/src/components/Header";
+import Sidebar from "@/src/components/Sidebar";
 
 export default async function AuthenticatedLayout({
   children,
@@ -13,5 +15,15 @@ export default async function AuthenticatedLayout({
     redirect("/login");
   }
 
-  return <LoggedInLayout>{children}</LoggedInLayout>;
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Header />
+      <Box sx={{ display: "flex", flexGrow: 1 }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {children}
+        </Box>
+      </Box>
+    </Box>
+  );
 }
