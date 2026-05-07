5e36e66ed05d780b820c5907e098976166ac4e36
Artem Petrenko <artem7888@gmail.com>
2026-05-07 15:17:15 +0300

    Update page.tsx

--- transaction-hub/src/app/(app)/batches/page.tsx
+++ transaction-hub/src/app/(app)/batches/page.tsx
@@ -1,7 +1,5 @@
-import { Typography } from "@mui/material";
+import Typography from "@/src/components/Typography";
 
 export default function BatchesPage() {
-  return (
-    <Typography variant="h4">Batches</Typography>
-  );
+  return <Typography variant="h4">Batches</Typography>;
 }
