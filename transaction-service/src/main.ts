c7007694f15dd077a1b7aa0059036cb51e8ac17b
Artem Petrenko <artem7888@gmail.com>
2026-05-13 21:46:50 +0300

    Update main.ts

--- transaction-service/src/main.ts
+++ transaction-service/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
+
+  app.use('/batches', express.json({ limit: '2.5mb' }));
 
   app.useGlobalPipes(
     new ValidationPipe({
